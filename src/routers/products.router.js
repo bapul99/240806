2e213058b7f9e181e8a732e6e04dd02bc14ab161
bapul <164993223+bapul99@users.noreply.github.com>
2024-08-13 09:00:19 +0900

    v3 수정

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -8,33 +8,43 @@ import { deleteJoiSchema } from '../middlewares/delete-joi.js';
 const productsRouter = express.Router();
 
 // 상품 생성 (CREATE)
-productsRouter.post('/products', createJoiSchema, async (req, res) => {
+productsRouter.post('/products', createJoiSchema, async (req, res, next) => {
+
+      try {
 
       // 상품 정보 파싱하기
       const { name, description, manager, password } = req.body;
 
+      // DB에서 조회하기
+      const existedProduct = await Product.findOne({ name });
+
+      if (existedProduct) {
+            return res
+                  .status(400)
+                  .json({ status: 400, message: '이미 등록 된 상품입니다.' });
+      }
+
       // DB에 저장하기
       const product = new Product({ name, description, manager, password });
-      
-      try {
-            let data = await product.save();
-            data = { ...data.toJSON(), password: undefined };
+      let data = await product.save();
+
+      data = { ...data.toJSON(), password: undefined };
 
       // 완료 메세지 반환하기
       return res
             .status(201)
             .json({ status: 201, message: '상품 생성에 성공했습니다.', data });
-
-      } catch (err) {
-            console.error(err);
-            return res.status(500).json({ message: '상품 생성에 실패했습니다.' });
+      } catch (error) {
+      next(error);
       }
 });
+
 
 // 상품 목록 조회 (READ)
 productsRouter.get('/products', async (req, res) => {
 
       try {
+
       // DB에서 상품 목록 조회하기
       const data = await Product.find().sort({ createdAt: 'desc' }).exec();
 
@@ -43,28 +53,35 @@ productsRouter.get('/products', async (req, res) => {
             .status(200)
             .json({ status: 200, message: '상품 목록 조회에 성공했습니다.', data });
 
-      } catch (err) {
-            return res.status(500).json({ message: '상품 목록 조회에 실패했습니다.' });
+      } catch (error) {
+      next(error);
       }
 });
 
 // 상품 상세 조회 (READ)
 productsRouter.get('/products/:id', async (req, res) => {
 
+      try {
+
       // 상품 ID 파싱하기
       const { id } = req.params;
 
       // DB에서 상품 상세 조회하기
-      try {
       const data = await Product.findById(id).exec();
+
+      if (!data) {
+            return res
+                  .status(404)
+                  .json({ status: 404, message: '상품이 존재하지 않습니다.' });
+      }
       
       // 완료 메세지 반환하기
       return res
             .status(200)
             .json({ status: 200, message: '상품 상세 조회에 성공했습니다.', data });
 
-      } catch (err) {
-            return res.status(500).json({ message: '상품 상세 조회에 실패했습니다.' });
+      } catch (error) {
+            next(error);
       }
 });
 
@@ -72,6 +89,7 @@ productsRouter.get('/products/:id', async (req, res) => {
 productsRouter.put('/products/:id', updateJoiSchema, async (req, res) => {
       
       try {
+
       // 상품 ID 파싱하기
       const { id } = req.params;
 
@@ -80,6 +98,12 @@ productsRouter.put('/products/:id', updateJoiSchema, async (req, res) => {
 
       // DB에서 조회하기
       const existedProduct = await Product.findById(id, { password: true });
+
+      if (!existedProduct) {
+            return res
+                  .status(404)
+                  .json({ status: 404, message: '상품이 존재하지 않습니다.' });
+      }
 
       // 비밀번호 일치 여부 확인
       const isPasswordMatched = password === existedProduct.password;
@@ -97,22 +121,26 @@ productsRouter.put('/products/:id', updateJoiSchema, async (req, res) => {
       };
 
       // DB에서 갱신하기
-      const data = await Product.findByIdAndUpdate(id, productInfo);
+      const data = await Product.findByIdAndUpdate(id, productInfo, {
+            new: true,
+      });
 
       // 완료 메세지 반환하기
       return res
             .status(200)
             .json({ status: 200, message: '상품 수정에 성공했습니다.', data });
             1
-      } catch (err) {
-            return res.status(500).json({ message: '상품 수정에 실패했습니다.' });
+      } catch (error) {
+            next(error);
       }
 });
 
 // 상품 삭제 (DELETE)
 
 productsRouter.delete('/products/:id', deleteJoiSchema, async (req, res) => {
+
       try {
+
       // 상품 ID 파싱하기
       const { id } = req.params;
 
@@ -121,6 +149,12 @@ productsRouter.delete('/products/:id', deleteJoiSchema, async (req, res) => {
 
       // DB에서 조회하기 (패스워드 포함)
       const existedProduct = await Product.findById(id, { password: true });
+
+      if (!existedProduct) {
+            return res
+                  .status(404)
+                  .json({ status: 404, message: '상품이 존재하지 않습니다.' });
+      }
 
       // 비밀번호 일치 여부 확인
       const isPasswordMatched = password === existedProduct.password;
@@ -138,8 +172,8 @@ productsRouter.delete('/products/:id', deleteJoiSchema, async (req, res) => {
       return res
       .status(200)
       .json({ status: 200, message: '상품 삭제에 성공했습니다.', data });
-      }  catch (err) {
-            return res.status(500).json({ message: '상품 삭제에 실패했습니다.' });
+      }  catch (error) {
+            next(error);
       }
 });
 
